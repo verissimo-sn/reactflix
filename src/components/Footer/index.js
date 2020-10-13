4bc88cee1c608901e02a4ae306c96ec9bbfe9756
verissimo-sn <verissimo_sn@hotmail.com>
2020-10-13 11:23:49 -0300

    fix(footer/link): fixing vulnerability target _blank in link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,11 +6,11 @@ const Footer = () => {
   return (
     <div className="credits" >
       <h4>
-        Criado por <a href="https://github.com/verissimo-sn" target="_blank" style={{ color: '#B2B200' }}>Verissimo-sn </a> 
+        Criado por <a rel="noopener noreferrer" href="https://github.com/verissimo-sn" target="_blank" style={{ color: '#B2B200' }}>Verissimo-sn </a> 
         inspirado na B7Web.
       </h4>
-      <h4>Direitos de imagem para <a href="https://www.netflix.com" target="_blank" style={{ color: '#E50914' }}>NetFlix</a></h4>
-      <h4>Dados utilizando api do <a href="https://www.themoviedb.org/" target="_blank" style={{ color: '#203354' }}>TMDB</a></h4>
+      <h4>Direitos de imagem para <a rel="noopener noreferrer" href="https://www.netflix.com" target="_blank" style={{ color: '#E50914' }}>NetFlix</a></h4>
+      <h4>Dados utilizando api do <a rel="noopener noreferrer" href="https://www.themoviedb.org/" target="_blank" style={{ color: '#203354' }}>TMDB</a></h4>
     </div>
   );
 }
